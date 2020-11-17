7c056c6b19a48ea586a6d78d74954eb4ab11886a
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-11-17 00:07:37 -0300

    Ensute that an object can't move to a negative position

--- src/test/java/org/rpgrunner/game/map/LayerTest.java
+++ src/test/java/org/rpgrunner/game/map/LayerTest.java
@@ -5,36 +5,55 @@ import java.util.Random;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import org.rpgrunner.game.Direction;
+import org.rpgrunner.test.mock.TileSetSpy;
+
 public class LayerTest extends TestCase {
-    public void testReturnSameTileMap() {
-        byte[][] tileMap = generateRandomTileMap();
-        Layer layer = new Layer(null, tileMap);
-        Assert.assertEquals(tileMap, layer.getTileMap());
-    }
+    private byte[][] tileMap;
 
-    public void testReturnSameWidthOfTileMap() {
-        byte[][] tileMap = generateRandomTileMap();
-        Layer layer = new Layer(null, tileMap);
-        Assert.assertEquals(tileMap[0].length, layer.getWidth());
-    }
-
-    public void testReturnSameHeightOfTileMap() {
-        byte[][] tileMap = generateRandomTileMap();
-        Layer layer = new Layer(null, tileMap);
-        Assert.assertEquals(tileMap.length, layer.getHeight());
+    public void setUp() {
+        tileMap = generateRandomTileMap();
     }
 
     private byte[][] generateRandomTileMap() {
         Random random = new Random();
         int height = random.nextInt(100) + 2;
         int width = random.nextInt(100) + 2;
-        byte[][] tileMap = new byte[height][width];
+        byte[][] newTileMap = new byte[height][width];
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                tileMap[y][x] = (byte) random.nextInt(256);
+                newTileMap[y][x] = (byte) random.nextInt(256);
             }
         }
-        return tileMap;
+        return newTileMap;
+    }
+
+    public void testReturnSameTileMap() {
+        Layer layer = new Layer(null, tileMap);
+        Assert.assertEquals(tileMap, layer.getTileMap());
+    }
+
+    public void testReturnSameWidthOfTileMap() {
+        Layer layer = new Layer(null, tileMap);
+        Assert.assertEquals(tileMap[0].length, layer.getWidth());
+    }
+
+    public void testReturnSameHeightOfTileMap() {
+        Layer layer = new Layer(null, tileMap);
+        Assert.assertEquals(tileMap.length, layer.getHeight());
+    }
+
+    public void testCantMoveToNegativePositions() {
+        boolean[] results = new boolean[] {false, false, false, false};
+        TileSetSpy tileSetSpy = new TileSetSpy(results);
+
+        Layer layerNegativeX = new Layer(tileSetSpy, tileMap);
+        Assert.assertFalse(
+            layerNegativeX.canMoveTo(0, 0, -1, 0, Direction.LEFT)
+        );
+
+        Layer layerNegativeY = new Layer(tileSetSpy, tileMap);
+        Assert.assertFalse(layerNegativeY.canMoveTo(0, 0, 0, -1, Direction.UP));
     }
 }
---
+++ src/test/java/org/rpgrunner/test/mock/TileSetSpy.java
@@ -0,0 +1,22 @@
+package org.rpgrunner.test.mock;
+
+import org.rpgrunner.game.tileset.TileSet;
+
+public class TileSetSpy extends TileSet {
+    private int resultsIndex;
+    private boolean[] results;
+
+    public TileSetSpy(final boolean[] spyResults) {
+        super(null, null);
+        resultsIndex = 0;
+        results = spyResults;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public boolean canCollideOn(final int tileIndex, final byte direction) {
+        return results[resultsIndex++];
+    }
+}
