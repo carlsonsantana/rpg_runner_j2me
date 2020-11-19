751f775ec4ee28304081d68985af6676eee3ff8e
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-11-19 01:21:05 -0300

    Ensure that a map returns the same width of layers passed on initializer

---
+++ src/test/java/org/rpgrunner/game/map/MapTest.java
@@ -0,0 +1,34 @@
+package org.rpgrunner.game.map;
+
+import java.util.Random;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.rpgrunner.test.mock.LayerSpy;
+
+public class MapTest extends TestCase {
+    private Map map;
+    private LayerSpy layerBackground;
+    private LayerSpy layerObjects;
+
+    public void setUp() {
+        layerBackground = new LayerSpy();
+        layerObjects = new LayerSpy();
+        Layer[] layers = new Layer[] {layerBackground, layerObjects};
+        map = new Map(layers);
+    }
+
+    public void testReturnSameWidthLayers() {
+        Random random = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            int width = random.nextInt(255);
+
+            layerBackground.setWidth(width);
+            layerObjects.setWidth(width);
+
+            Assert.assertEquals(width, map.getWidth());
+        }
+    }
+}
---
+++ src/test/java/org/rpgrunner/test/mock/LayerSpy.java
@@ -0,0 +1,52 @@
+package org.rpgrunner.test.mock;
+
+import org.rpgrunner.game.map.Layer;
+import org.rpgrunner.game.tileset.TileSet;
+
+public class LayerSpy extends Layer {
+    private int width;
+    private int height;
+    private boolean canMove;
+
+    public LayerSpy() {
+        super(null, null);
+    }
+
+    public void setWidth(final int newWidth) {
+        width = newWidth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setHeight(final int newHeight) {
+        height = newHeight;
+    }
+
+    public int getHeight() {
+        return 0;
+    }
+
+    public byte[][] getTileMap() {
+        return null;
+    }
+
+    public TileSet getTileSet() {
+        return null;
+    }
+
+    public void setCanMove(final boolean newCanMove) {
+        canMove = newCanMove;
+    }
+
+    public boolean canMoveTo(
+        final int fromX,
+        final int fromY,
+        final int toX,
+        final int toY,
+        final byte direction
+    ) {
+        return canMove;
+    }
+}
