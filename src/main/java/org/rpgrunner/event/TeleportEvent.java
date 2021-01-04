2b7554f1585f24efa45736cad9b130dad234da2d
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-01-03 21:22:02 -0300

    Set all TeleportEvent properties as final

--- src/main/java/org/rpgrunner/event/TeleportEvent.java
+++ src/main/java/org/rpgrunner/event/TeleportEvent.java
@@ -5,8 +5,8 @@ import org.rpgrunner.map.Map;
 import org.rpgrunner.map.MapLoader;
 
 public class TeleportEvent {
-    private GameController gameController;
-    private String mapName;
+    private final GameController gameController;
+    private final String mapName;
 
     public TeleportEvent(
         final GameController currentGameController,
