bec6b8eb2c63dd35e9421ca48fbe3b8182675f43
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-01-06 01:55:21 -0300

    Create an action to teleport character to another map location

---
+++ src/test/java/org/rpgrunner/event/action/LocalTeleportTest.java
@@ -0,0 +1,45 @@
+package org.rpgrunner.event.action;
+
+import java.util.Random;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.rpgrunner.event.action.LocalTeleport;
+import org.rpgrunner.character.CharacterElement;
+import org.rpgrunner.test.mock.SimpleCharacter;
+import org.rpgrunner.test.mock.CharacterAnimationSpy;
+
+public class LocalTeleportTest extends TestCase {
+    public void testChangeCharacterPosition() {
+        Random random = new Random();
+        SimpleCharacter character = new SimpleCharacter();
+        CharacterAnimationSpy characterAnimation = new CharacterAnimationSpy();
+        CharacterElement characterElement = new CharacterElement(
+            null,
+            character,
+            characterAnimation,
+            null
+        );
+
+        for (int i = 0; i < 100; i++) {
+            int mapPositionX = random.nextInt(1000);
+            int mapPositionY = random.nextInt(1000);
+            LocalTeleport localTeleport = new LocalTeleport(
+                mapPositionX,
+                mapPositionY
+            );
+
+            localTeleport.setCharacterElement(characterElement);
+            localTeleport.call();
+
+            Assert.assertTrue(
+                characterAnimation.isUpdateScreenPositionFromMapPositionCalled()
+            );
+            Assert.assertEquals(mapPositionX, character.getMapPositionX());
+            Assert.assertEquals(mapPositionY, character.getMapPositionY());
+            Assert.assertEquals(mapPositionX, character.getMapNextPositionX());
+            Assert.assertEquals(mapPositionY, character.getMapNextPositionY());
+        }
+    }
+}
--- src/test/java/org/rpgrunner/test/mock/CharacterAnimationSpy.java
+++ src/test/java/org/rpgrunner/test/mock/CharacterAnimationSpy.java
@@ -7,11 +7,13 @@ public class CharacterAnimationSpy implements CharacterAnimation {
     private boolean startAnimationCalled;
     private int screenX;
     private int screenY;
+    private boolean updateScreenPositionFromMapPositionCalled;
 
     public CharacterAnimationSpy() {
         startAnimationCalled = false;
         screenX = 0;
         screenY = 0;
+        updateScreenPositionFromMapPositionCalled = false;
     }
 
     public int getScreenX() {
@@ -22,7 +24,13 @@ public class CharacterAnimationSpy implements CharacterAnimation {
         return screenY;
     }
 
-    public void updateScreenPositionFromMapPosition() { }
+    public void updateScreenPositionFromMapPosition() {
+        updateScreenPositionFromMapPositionCalled = true;
+    }
+
+    public boolean isUpdateScreenPositionFromMapPositionCalled() {
+        return updateScreenPositionFromMapPositionCalled;
+    }
 
     public void setScreenPosition(final int newScreenX, final int newScreenY) {
         screenX = newScreenX;
