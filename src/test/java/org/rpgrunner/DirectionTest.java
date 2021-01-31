d853ef76c3ec7e2656cfdb2a2f9fb570c06f3a36
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-01-30 21:54:12 -0300

    Refactor DirectorionTest class to simplify it

--- src/test/java/org/rpgrunner/DirectionTest.java
+++ src/test/java/org/rpgrunner/DirectionTest.java
@@ -33,56 +33,29 @@ public class DirectionTest extends TestCase {
     }
 
     public void testDetectInvertDirection() {
-        Assert.assertFalse(
-            Direction.isUp(Direction.invertDirection(Direction.UP))
-        );
-        Assert.assertFalse(
-            Direction.isUp(Direction.invertDirection(Direction.RIGHT))
-        );
-        Assert.assertTrue(
-            Direction.isUp(Direction.invertDirection(Direction.DOWN))
-        );
-        Assert.assertFalse(
-            Direction.isUp(Direction.invertDirection(Direction.LEFT))
-        );
+        byte invertedUp = Direction.invertDirection(Direction.UP);
+        byte invertedRight = Direction.invertDirection(Direction.RIGHT);
+        byte invertedDown = Direction.invertDirection(Direction.DOWN);
+        byte invertedLeft = Direction.invertDirection(Direction.LEFT);
 
-        Assert.assertFalse(
-            Direction.isRight(Direction.invertDirection(Direction.UP))
-        );
-        Assert.assertFalse(
-            Direction.isRight(Direction.invertDirection(Direction.RIGHT))
-        );
-        Assert.assertFalse(
-            Direction.isRight(Direction.invertDirection(Direction.DOWN))
-        );
-        Assert.assertTrue(
-            Direction.isRight(Direction.invertDirection(Direction.LEFT))
-        );
+        Assert.assertFalse(Direction.isUp(invertedUp));
+        Assert.assertFalse(Direction.isUp(invertedRight));
+        Assert.assertTrue(Direction.isUp(invertedDown));
+        Assert.assertFalse(Direction.isUp(invertedLeft));
 
-        Assert.assertTrue(
-            Direction.isDown(Direction.invertDirection(Direction.UP))
-        );
-        Assert.assertFalse(
-            Direction.isDown(Direction.invertDirection(Direction.RIGHT))
-        );
-        Assert.assertFalse(
-            Direction.isDown(Direction.invertDirection(Direction.DOWN))
-        );
-        Assert.assertFalse(
-            Direction.isDown(Direction.invertDirection(Direction.LEFT))
-        );
+        Assert.assertFalse(Direction.isRight(invertedUp));
+        Assert.assertFalse(Direction.isRight(invertedRight));
+        Assert.assertFalse(Direction.isRight(invertedDown));
+        Assert.assertTrue(Direction.isRight(invertedLeft));
 
-        Assert.assertFalse(
-            Direction.isLeft(Direction.invertDirection(Direction.UP))
-        );
-        Assert.assertTrue(
-            Direction.isLeft(Direction.invertDirection(Direction.RIGHT))
-        );
-        Assert.assertFalse(
-            Direction.isLeft(Direction.invertDirection(Direction.DOWN))
-        );
-        Assert.assertFalse(
-            Direction.isLeft(Direction.invertDirection(Direction.LEFT))
-        );
+        Assert.assertTrue(Direction.isDown(invertedUp));
+        Assert.assertFalse(Direction.isDown(invertedRight));
+        Assert.assertFalse(Direction.isDown(invertedDown));
+        Assert.assertFalse(Direction.isDown(invertedLeft));
+
+        Assert.assertFalse(Direction.isLeft(invertedUp));
+        Assert.assertTrue(Direction.isLeft(invertedRight));
+        Assert.assertFalse(Direction.isLeft(invertedDown));
+        Assert.assertFalse(Direction.isLeft(invertedLeft));
     }
 }
