dcda8a6981d03ad1a0645b6bceab328e80bfed3c
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-02-02 22:33:44 -0300

    Create methods to test each loop iteration for map tests

--- src/test/java/org/rpgrunner/map/MapTest.java
+++ src/test/java/org/rpgrunner/map/MapTest.java
@@ -10,6 +10,7 @@ import org.rpgrunner.test.helper.RandomGenerator;
 import org.rpgrunner.test.mock.LayerSpy;
 
 public class MapTest extends TestCase {
+    private static int TEST_REPEAT_LOOP = 100;
     private Random random;
     private Map map;
     private String mapFileBaseName;
@@ -29,55 +30,66 @@ public class MapTest extends TestCase {
         map = new Map(mapFileBaseName, layers);
     }
 
-    public void testReturnSame() {
+    public void testReturnSameMapFileBaseName() {
         Assert.assertSame(mapFileBaseName, map.getFileBaseName());
     }
 
-    public void testReturnSameWidthLayers() {
-        for (int i = 0; i < 100; i++) {
-            int width = random.nextInt(255);
-
-            layerBackground.setWidth(width);
-            layerObjects.setWidth(width);
-
-            Assert.assertEquals(width, map.getWidth());
+    public void testReturnSameWidthLayersLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkReturnSameWidthLayers();
         }
     }
 
-    public void testReturnSameHeightLayers() {
-        for (int i = 0; i < 100; i++) {
-            int height = random.nextInt(255);
+    private void checkReturnSameWidthLayers() {
+        int width = random.nextInt(255);
 
-            layerBackground.setHeight(height);
-            layerObjects.setHeight(height);
+        layerBackground.setWidth(width);
+        layerObjects.setWidth(width);
 
-            Assert.assertEquals(height, map.getHeight());
+        Assert.assertEquals(width, map.getWidth());
+    }
+
+    public void testReturnSameHeightLayersLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkReturnSameHeightLayers();
         }
+    }
+
+    private void checkReturnSameHeightLayers() {
+        int height = random.nextInt(255);
+
+        layerBackground.setHeight(height);
+        layerObjects.setHeight(height);
+
+        Assert.assertEquals(height, map.getHeight());
     }
 
     public void testReturnSameLayers() {
         Assert.assertSame(layers, map.getLayers());
     }
 
-    public void testCanMoveToPositionWhenAllLayersAllMoveToPosition() {
-        for (int i = 0; i < 100; i++) {
-            boolean canMoveToBackground = random.nextInt(2) == 1;
-            boolean canMoveToObjects = random.nextInt(2) == 1;
-
-            layerBackground.setCanMove(canMoveToBackground);
-            layerObjects.setCanMove(canMoveToObjects);
-
-            boolean canMove = canMoveToBackground && canMoveToObjects;
-            int fromX = random.nextInt(100);
-            int fromY = random.nextInt(100);
-            int toX = random.nextInt(100);
-            int toY = random.nextInt(100);
-            byte direction = Direction.LEFT;
-
-            Assert.assertEquals(
-                canMove,
-                map.canMoveTo(fromX, fromY, toX, toY, direction)
-            );
+    public void testCanMoveToPositionWhenAllLayersAllMoveToPositionLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkCanMoveToPositionWhenAllLayersAllMoveToPosition();
         }
+    }
+
+    private void checkCanMoveToPositionWhenAllLayersAllMoveToPosition() {
+        boolean canMoveToBackground = random.nextInt(2) == 1;
+        boolean canMoveToObjects = random.nextInt(2) == 1;
+        boolean canMove = canMoveToBackground && canMoveToObjects;
+        int fromX = random.nextInt(100);
+        int fromY = random.nextInt(100);
+        int toX = random.nextInt(100);
+        int toY = random.nextInt(100);
+        byte direction = Direction.LEFT;
+
+        layerBackground.setCanMove(canMoveToBackground);
+        layerObjects.setCanMove(canMoveToObjects);
+
+        Assert.assertEquals(
+            canMove,
+            map.canMoveTo(fromX, fromY, toX, toY, direction)
+        );
     }
 }
