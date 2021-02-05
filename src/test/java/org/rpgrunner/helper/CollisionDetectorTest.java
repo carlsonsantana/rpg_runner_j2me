7b100a71bc8f967db2a662072db67989e73c512c
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-02-05 19:54:16 -0300

    Refactor loop test method of CollisionDetectorTest to follow the pattern

--- src/test/java/org/rpgrunner/helper/CollisionDetectorTest.java
+++ src/test/java/org/rpgrunner/helper/CollisionDetectorTest.java
@@ -12,6 +12,7 @@ import org.rpgrunner.test.helper.TestCollisionAllDirections;
 import org.rpgrunner.test.mock.map.MapSpy;
 
 public class CollisionDetectorTest extends TestCase {
+    private static int TEST_REPEAT_LOOP = 100;
     private CollisionDetector collisionDetector;
 
     public void setUp() {
@@ -36,147 +37,170 @@ public class CollisionDetectorTest extends TestCase {
         Assert.assertFalse(collisionDetector.canMove(character));
     }
 
-    public void testCantMoveUpWhenExistsACharacterCollision() {
-        TestCollisionAllDirections test;
-
-        for (int i = 0; i < 100; i++) {
-            test = new TestCollisionAllDirections(collisionDetector, 0, -1) {
-                public void moveCharacter(final GameCharacter characterTest) {
-                    characterTest.moveUp();
-                }
-
-                public void testOperation(final GameCharacter characterTest) {
-                    Assert.assertFalse(
-                        collisionDetector.canMove(characterTest)
-                    );
-                }
-            };
-            test.test();
+    public void testCantMoveUpWhenExistsACharacterCollisionLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkCantMoveUpWhenExistsACharacterCollision();
         }
     }
 
-    public void testCantMoveRightWhenExistsACharacterCollision() {
+    private void checkCantMoveUpWhenExistsACharacterCollision() {
         TestCollisionAllDirections test;
+        test = new TestCollisionAllDirections(collisionDetector, 0, -1) {
+            public void moveCharacter(final GameCharacter characterTest) {
+                characterTest.moveUp();
+            }
 
-        for (int i = 0; i < 100; i++) {
-            test = new TestCollisionAllDirections(collisionDetector, 1, 0) {
-                public void moveCharacter(final GameCharacter characterTest) {
-                    characterTest.moveRight();
-                }
+            public void testOperation(final GameCharacter characterTest) {
+                Assert.assertFalse(collisionDetector.canMove(characterTest));
+            }
+        };
+        test.test();
+    }
 
-                public void testOperation(final GameCharacter characterTest) {
-                    Assert.assertFalse(
-                        collisionDetector.canMove(characterTest)
-                    );
-                }
-            };
-            test.test();
+    public void testCantMoveRightWhenExistsACharacterCollisionLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkCantMoveRightWhenExistsACharacterCollision();
         }
     }
 
-    public void testCantMoveDownWhenExistsACharacterCollision() {
+    private void checkCantMoveRightWhenExistsACharacterCollision() {
         TestCollisionAllDirections test;
 
-        for (int i = 0; i < 100; i++) {
-            test = new TestCollisionAllDirections(collisionDetector, 0, 1) {
-                public void moveCharacter(final GameCharacter characterTest) {
-                    characterTest.moveDown();
-                }
+        test = new TestCollisionAllDirections(collisionDetector, 1, 0) {
+            public void moveCharacter(final GameCharacter characterTest) {
+                characterTest.moveRight();
+            }
 
-                public void testOperation(final GameCharacter characterTest) {
-                    Assert.assertFalse(
-                        collisionDetector.canMove(characterTest)
-                    );
-                }
-            };
-            test.test();
+            public void testOperation(final GameCharacter characterTest) {
+                Assert.assertFalse(collisionDetector.canMove(characterTest));
+            }
+        };
+        test.test();
+    }
+
+    public void testCantMoveDownWhenExistsACharacterCollisionLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkCantMoveDownWhenExistsACharacterCollision();
         }
     }
 
-    public void testCantMoveLeftWhenExistsACharacterCollision() {
+    private void checkCantMoveDownWhenExistsACharacterCollision() {
         TestCollisionAllDirections test;
 
-        for (int i = 0; i < 100; i++) {
-            test = new TestCollisionAllDirections(collisionDetector, -1, 0) {
-                public void moveCharacter(final GameCharacter characterTest) {
-                    characterTest.moveLeft();
-                }
+        test = new TestCollisionAllDirections(collisionDetector, 0, 1) {
+            public void moveCharacter(final GameCharacter characterTest) {
+                characterTest.moveDown();
+            }
 
-                public void testOperation(final GameCharacter characterTest) {
-                    Assert.assertFalse(
-                        collisionDetector.canMove(characterTest)
-                    );
-                }
-            };
-            test.test();
+            public void testOperation(final GameCharacter characterTest) {
+                Assert.assertFalse(collisionDetector.canMove(characterTest));
+            }
+        };
+        test.test();
+    }
+
+    public void testCantMoveLeftWhenExistsACharacterCollisionLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkCantMoveLeftWhenExistsACharacterCollision();
         }
     }
 
-    public void testCanMoveUpWhenNotExistsACharacterCollision() {
+    private void checkCantMoveLeftWhenExistsACharacterCollision() {
         TestCollisionAllDirections test;
 
-        for (int i = 0; i < 100; i++) {
-            test = new TestCollisionAllDirections(collisionDetector, 0, -2) {
-                public void moveCharacter(final GameCharacter characterTest) {
-                    characterTest.moveUp();
-                }
+        test = new TestCollisionAllDirections(collisionDetector, -1, 0) {
+            public void moveCharacter(final GameCharacter characterTest) {
+                characterTest.moveLeft();
+            }
 
-                public void testOperation(final GameCharacter characterTest) {
-                    Assert.assertTrue(collisionDetector.canMove(characterTest));
-                }
-            };
-            test.test();
+            public void testOperation(final GameCharacter characterTest) {
+                Assert.assertFalse(collisionDetector.canMove(characterTest));
+            }
+        };
+        test.test();
+    }
+
+    public void testCanMoveUpWhenNotExistsACharacterCollisionLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkCanMoveUpWhenNotExistsACharacterCollision();
         }
     }
 
-    public void testCanMoveRightWhenNotExistsACharacterCollision() {
+    private void checkCanMoveUpWhenNotExistsACharacterCollision() {
         TestCollisionAllDirections test;
 
-        for (int i = 0; i < 100; i++) {
-            test = new TestCollisionAllDirections(collisionDetector, 2, 0) {
-                public void moveCharacter(final GameCharacter characterTest) {
-                    characterTest.moveRight();
-                }
+        test = new TestCollisionAllDirections(collisionDetector, 0, -2) {
+            public void moveCharacter(final GameCharacter characterTest) {
+                characterTest.moveUp();
+            }
 
-                public void testOperation(final GameCharacter characterTest) {
-                    Assert.assertTrue(collisionDetector.canMove(characterTest));
-                }
-            };
-            test.test();
+            public void testOperation(final GameCharacter characterTest) {
+                Assert.assertTrue(collisionDetector.canMove(characterTest));
+            }
+        };
+        test.test();
+    }
+
+    public void testCanMoveRightWhenNotExistsACharacterCollisionLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkCanMoveRightWhenNotExistsACharacterCollision();
         }
     }
 
-    public void testCanMoveDownWhenNotExistsACharacterCollision() {
+    private void checkCanMoveRightWhenNotExistsACharacterCollision() {
         TestCollisionAllDirections test;
 
-        for (int i = 0; i < 100; i++) {
-            test = new TestCollisionAllDirections(collisionDetector, 0, 2) {
-                public void moveCharacter(final GameCharacter characterTest) {
-                    characterTest.moveDown();
-                }
+        test = new TestCollisionAllDirections(collisionDetector, 2, 0) {
+            public void moveCharacter(final GameCharacter characterTest) {
+                characterTest.moveRight();
+            }
 
-                public void testOperation(final GameCharacter characterTest) {
-                    Assert.assertTrue(collisionDetector.canMove(characterTest));
-                }
-            };
-            test.test();
+            public void testOperation(final GameCharacter characterTest) {
+                Assert.assertTrue(collisionDetector.canMove(characterTest));
+            }
+        };
+        test.test();
+    }
+
+    public void testCanMoveDownWhenNotExistsACharacterCollisionLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkCanMoveDownWhenNotExistsACharacterCollision();
         }
     }
 
-    public void testCanMoveLeftWhenNotExistsACharacterCollision() {
+    private void checkCanMoveDownWhenNotExistsACharacterCollision() {
         TestCollisionAllDirections test;
 
-        for (int i = 0; i < 100; i++) {
-            test = new TestCollisionAllDirections(collisionDetector, -2, 0) {
-                public void moveCharacter(final GameCharacter characterTest) {
-                    characterTest.moveLeft();
-                }
+        test = new TestCollisionAllDirections(collisionDetector, 0, 2) {
+            public void moveCharacter(final GameCharacter characterTest) {
+                characterTest.moveDown();
+            }
 
-                public void testOperation(final GameCharacter characterTest) {
-                    Assert.assertTrue(collisionDetector.canMove(characterTest));
-                }
-            };
-            test.test();
+            public void testOperation(final GameCharacter characterTest) {
+                Assert.assertTrue(collisionDetector.canMove(characterTest));
+            }
+        };
+        test.test();
+    }
+
+    public void testCanMoveLeftWhenNotExistsACharacterCollisionLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkCanMoveLeftWhenNotExistsACharacterCollision();
         }
+    }
+
+    private void checkCanMoveLeftWhenNotExistsACharacterCollision() {
+        TestCollisionAllDirections test;
+
+        test = new TestCollisionAllDirections(collisionDetector, -2, 0) {
+            public void moveCharacter(final GameCharacter characterTest) {
+                characterTest.moveLeft();
+            }
+
+            public void testOperation(final GameCharacter characterTest) {
+                Assert.assertTrue(collisionDetector.canMove(characterTest));
+            }
+        };
+        test.test();
     }
 }
