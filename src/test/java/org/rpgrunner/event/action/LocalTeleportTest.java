c683473e113c53578a882a8d5eb7fcad9aca3543
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-02-05 01:58:05 -0300

    Refactor loop test method of LocalTeleportTest to follow the pattern

--- src/test/java/org/rpgrunner/event/action/LocalTeleportTest.java
+++ src/test/java/org/rpgrunner/event/action/LocalTeleportTest.java
@@ -11,35 +11,50 @@ import org.rpgrunner.test.mock.character.CharacterAnimationSpy;
 import org.rpgrunner.test.mock.character.SimpleCharacter;
 
 public class LocalTeleportTest extends TestCase {
-    public void testChangeCharacterPosition() {
-        Random random = new Random();
-        SimpleCharacter character = new SimpleCharacter();
-        CharacterAnimationSpy characterAnimation = new CharacterAnimationSpy();
-        CharacterElement characterElement = new CharacterElement(
+    private static int TEST_REPEAT_LOOP = 100;
+    private Random random;
+    private SimpleCharacter character;
+    private CharacterAnimationSpy characterAnimation;
+    private CharacterElement characterElement;
+
+    public LocalTeleportTest() {
+        random = new Random();
+    }
+
+    public void setUp() {
+        character = new SimpleCharacter();
+        characterAnimation = new CharacterAnimationSpy();
+        characterElement = new CharacterElement(
             null,
             character,
             characterAnimation,
             null
         );
+    }
 
-        for (int i = 0; i < 100; i++) {
-            int mapPositionX = random.nextInt(1000);
-            int mapPositionY = random.nextInt(1000);
-            LocalTeleport localTeleport = new LocalTeleport(
-                mapPositionX,
-                mapPositionY
-            );
-
-            localTeleport.setCharacterElement(characterElement);
-            localTeleport.execute();
-
-            Assert.assertTrue(
-                characterAnimation.isUpdateScreenPositionFromMapPositionCalled()
-            );
-            Assert.assertEquals(mapPositionX, character.getMapPositionX());
-            Assert.assertEquals(mapPositionY, character.getMapPositionY());
-            Assert.assertEquals(mapPositionX, character.getMapNextPositionX());
-            Assert.assertEquals(mapPositionY, character.getMapNextPositionY());
+    public void testChangeCharacterPositionLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkChangeCharacterPosition();
         }
+    }
+
+    private void checkChangeCharacterPosition() {
+        int mapPositionX = random.nextInt(1000);
+        int mapPositionY = random.nextInt(1000);
+        LocalTeleport localTeleport = new LocalTeleport(
+            mapPositionX,
+            mapPositionY
+        );
+
+        localTeleport.setCharacterElement(characterElement);
+        localTeleport.execute();
+
+        Assert.assertTrue(
+            characterAnimation.isUpdateScreenPositionFromMapPositionCalled()
+        );
+        Assert.assertEquals(mapPositionX, character.getMapPositionX());
+        Assert.assertEquals(mapPositionY, character.getMapPositionY());
+        Assert.assertEquals(mapPositionX, character.getMapNextPositionX());
+        Assert.assertEquals(mapPositionY, character.getMapNextPositionY());
     }
 }
