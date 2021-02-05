c3f9adada695bd1d349c634cb10e737d81d7512a
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-02-05 02:04:29 -0300

    Refactor loop test method of PlayerCharacterCreatorTest to follow the pattern

--- src/test/java/org/rpgrunner/event/action/PlayerCharacterCreatorTest.java
+++ src/test/java/org/rpgrunner/event/action/PlayerCharacterCreatorTest.java
@@ -14,52 +14,51 @@ import org.rpgrunner.test.helper.RandomGenerator;
 import org.rpgrunner.test.mock.GameControllerSpy;
 
 public class PlayerCharacterCreatorTest extends TestCase {
+    private static int TEST_REPEAT_LOOP = 100;
     private Random random;
 
     public PlayerCharacterCreatorTest() {
         random = new Random();
     }
 
-    public void testCreatePlayerCharacter() {
-        GameController gameController;
-        String randomFileBaseName;
-        int initialMapPositionX;
-        int initialMapPositionY;
-        PlayerCharacterCreator playerCharacterCreator;
+    public void testCreatePlayerCharacterLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkCreatePlayerCharacter();
+        }
+    }
 
-        for (int i = 0; i < 100; i++) {
-            gameController = new GameControllerSpy();
-            randomFileBaseName = RandomGenerator.getRandomString();
-            initialMapPositionX = random.nextInt(100) + 2;
-            initialMapPositionY = random.nextInt(100) + 2;
-            playerCharacterCreator = new PlayerCharacterCreator(
+    private void checkCreatePlayerCharacter() {
+        GameController gameController = new GameControllerSpy();
+        String randomFileBaseName = RandomGenerator.getRandomString();
+        int initialMapPositionX = random.nextInt(100) + 2;
+        int initialMapPositionY = random.nextInt(100) + 2;
+        PlayerCharacterCreator playerCharacterCreator = (
+            new PlayerCharacterCreator(
                 gameController,
                 randomFileBaseName,
                 initialMapPositionX,
                 initialMapPositionY
-            );
+            )
+        );
 
-            playerCharacterCreator.execute();
+        playerCharacterCreator.execute();
 
-            CharacterElement playerCharacterElement = (
-                gameController.getPlayerCharacterElement()
-            );
-            GameCharacter playerCharacter = (
-                playerCharacterElement.getCharacter()
-            );
+        CharacterElement playerCharacterElement = (
+            gameController.getPlayerCharacterElement()
+        );
+        GameCharacter playerCharacter = playerCharacterElement.getCharacter();
 
-            Assert.assertSame(
-                randomFileBaseName,
-                playerCharacter.getFileBaseName()
-            );
-            Assert.assertEquals(
-                initialMapPositionX,
-                playerCharacter.getMapPositionX()
-            );
-            Assert.assertEquals(
-                initialMapPositionY,
-                playerCharacter.getMapPositionY()
-            );
-        }
+        Assert.assertSame(
+            randomFileBaseName,
+            playerCharacter.getFileBaseName()
+        );
+        Assert.assertEquals(
+            initialMapPositionX,
+            playerCharacter.getMapPositionX()
+        );
+        Assert.assertEquals(
+            initialMapPositionY,
+            playerCharacter.getMapPositionY()
+        );
     }
 }
