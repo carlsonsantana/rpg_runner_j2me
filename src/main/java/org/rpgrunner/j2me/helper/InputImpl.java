09c1374c151ddd4ca8a812ed38e7598711207c81
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-10-16 17:53:41 -0300

    Move Key static methods to InputImpl

--- src/main/java/org/rpgrunner/j2me/helper/InputImpl.java
+++ src/main/java/org/rpgrunner/j2me/helper/InputImpl.java
@@ -1,9 +1,30 @@
 package org.rpgrunner.j2me.helper;
 
+import javax.microedition.lcdui.game.GameCanvas;
+
 import org.rpgrunner.helper.Input;
-import org.rpgrunner.j2me.Key;
 
 public class InputImpl implements Input {
+    private static final int[] ACTION_KEYS = new int[] {
+        GameCanvas.FIRE,
+        GameCanvas.KEY_NUM5
+    };
+    private static final int[] UP_KEYS = new int[] {
+        GameCanvas.UP,
+        GameCanvas.KEY_NUM2
+    };
+    private static final int[] RIGHT_KEYS = new int[] {
+        GameCanvas.RIGHT,
+        GameCanvas.KEY_NUM6
+    };
+    private static final int[] DOWN_KEYS = new int[] {
+        GameCanvas.DOWN,
+        GameCanvas.KEY_NUM8
+    };
+    private static final int[] LEFT_KEYS = new int[] {
+        GameCanvas.LEFT,
+        GameCanvas.KEY_NUM4
+    };
     private boolean actionPressed;
     private boolean holdingUp;
     private boolean holdingRight;
@@ -31,29 +52,53 @@ public class InputImpl implements Input {
     }
 
     public void pressKey(final int key) {
-        if (Key.isUp(key)) {
+        if (isUp(key)) {
             holdingUp = true;
-        } else if (Key.isRight(key)) {
+        } else if (isRight(key)) {
             holdingRight = true;
-        } else if (Key.isDown(key)) {
+        } else if (isDown(key)) {
             holdingDown = true;
-        } else if (Key.isLeft(key)) {
+        } else if (isLeft(key)) {
             holdingLeft = true;
         }
     }
 
     public void releaseKey(final int key) {
-        if (Key.isAction(key)) {
+        if (isAction(key)) {
             actionPressed = true;
-        } else if (Key.isUp(key)) {
+        } else if (isUp(key)) {
             holdingUp = false;
-        } else if (Key.isRight(key)) {
+        } else if (isRight(key)) {
             holdingRight = false;
-        } else if (Key.isDown(key)) {
+        } else if (isDown(key)) {
             holdingDown = false;
-        } else if (Key.isLeft(key)) {
+        } else if (isLeft(key)) {
             holdingLeft = false;
         }
+    }
+
+    private boolean isAction(final int key) {
+        return containsKey(ACTION_KEYS, key);
+    }
+
+    private boolean isUp(final int key) {
+        return containsKey(UP_KEYS, key);
+    }
+
+    private boolean isRight(final int key) {
+        return containsKey(RIGHT_KEYS, key);
+    }
+
+    private boolean isDown(final int key) {
+        return containsKey(DOWN_KEYS, key);
+    }
+
+    private boolean isLeft(final int key) {
+        return containsKey(LEFT_KEYS, key);
+    }
+
+    private boolean containsKey(final int[] keys, final int key) {
+        return (keys[0] == key) || (keys[1] == key);
     }
 
     public void update() {
