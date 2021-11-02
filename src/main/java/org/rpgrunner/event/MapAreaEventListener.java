7139d28f67574e59e9df57ab51a972c6db23fb40
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-11-01 22:58:37 -0300

    Allow interact with MapAreaEventListener

--- src/main/java/org/rpgrunner/event/MapAreaEventListener.java
+++ src/main/java/org/rpgrunner/event/MapAreaEventListener.java
@@ -1,21 +1,28 @@
 package org.rpgrunner.event;
 
+import org.rpgrunner.event.action.Action;
+import org.rpgrunner.event.action.NullAction;
+
 public class MapAreaEventListener {
+    private static final NullAction NULL_ACTION = new NullAction();
     private final int x;
     private final int y;
     private final int width;
     private final int height;
+    private final MapEvent[] events;
 
     public MapAreaEventListener(
         final int tilePositionX,
         final int tilePositionY,
         final int tilesWidth,
-        final int tilesHeight
+        final int tilesHeight,
+        final MapEvent[] eventsList
     ) {
         x = tilePositionX;
         y = tilePositionY;
         width = tilesWidth;
         height = tilesHeight;
+        events = eventsList;
     }
 
     public int getX() {
@@ -32,5 +39,19 @@ public class MapAreaEventListener {
 
     public int getHeight() {
         return height;
+    }
+
+    public Action interact(final byte direction) {
+        for (int i = 0, length = events.length; i < length; i++) {
+            MapEvent event = events[i];
+
+            Action action = event.interact(direction);
+
+            if (action != null) {
+                return action;
+            }
+        }
+
+        return NULL_ACTION;
     }
 }
--- src/test/java/org/rpgrunner/event/MapAreaEventListenerTest.java
+++ src/test/java/org/rpgrunner/event/MapAreaEventListenerTest.java
@@ -5,7 +5,16 @@ import java.util.Random;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import org.rpgrunner.Direction;
+import org.rpgrunner.event.action.Action;
+import org.rpgrunner.event.action.NullAction;
+import org.rpgrunner.test.mock.event.MapEventSpy;
+import org.rpgrunner.test.mock.event.action.ActionSpy;
+
 public class MapAreaEventListenerTest extends TestCase {
+    private static final int TEST_REPEAT_LOOP = 100;
+    private static final int MAX_ARRAY_SIZE = 100;
+    private static final int MIN_ARRAY_SIZE = 10;
     private final Random random;
 
     public MapAreaEventListenerTest() {
@@ -22,12 +31,86 @@ public class MapAreaEventListenerTest extends TestCase {
             x,
             y,
             width,
-            height
+            height,
+            null
         );
 
         Assert.assertEquals(x, mapAreaEventListener.getX());
         Assert.assertEquals(y, mapAreaEventListener.getY());
         Assert.assertEquals(width, mapAreaEventListener.getWidth());
         Assert.assertEquals(height, mapAreaEventListener.getHeight());
+    }
+
+    public void testReturnsNullActionWhenNotAreEvents() {
+        MapEvent[] emptyMapEvents = new MapEvent[0];
+        MapAreaEventListener mapAreaEventListener = new MapAreaEventListener(
+            0,
+            0,
+            0,
+            0,
+            emptyMapEvents
+        );
+
+        Action action = mapAreaEventListener.interact(Direction.UP);
+        Assert.assertTrue(action instanceof NullAction);
+    }
+
+    public void testReturnsNullActionWhenNotAreEventsMatching() {
+        MapEventSpy mapEvent = new MapEventSpy();
+        MapEvent[] mapEvents = new MapEvent[] {mapEvent};
+        MapAreaEventListener mapAreaEventListener = new MapAreaEventListener(
+            0,
+            0,
+            0,
+            0,
+            mapEvents
+        );
+
+        Action action = mapAreaEventListener.interact(Direction.UP);
+        Assert.assertTrue(action instanceof NullAction);
+    }
+
+    public void testReturnsFirstNotNullActionLoop() {
+        for (int i = 0; i < TEST_REPEAT_LOOP; i++) {
+            checkReturnsFirstNotNullAction();
+        }
+    }
+
+    private void checkReturnsFirstNotNullAction() {
+        ActionSpy expectedAction = new ActionSpy();
+        MapEventSpy mapEvent = new MapEventSpy();
+        mapEvent.setInteractAction(expectedAction);
+        MapEvent[] mapEvents = getRandomMapEvents(mapEvent);
+        MapAreaEventListener mapAreaEventListener = new MapAreaEventListener(
+            0,
+            0,
+            0,
+            0,
+            mapEvents
+        );
+
+        Action action = mapAreaEventListener.interact(Direction.UP);
+        Assert.assertEquals(expectedAction, action);
+    }
+
+    private MapEvent[] getRandomMapEvents(final MapEvent expectedMapEvent) {
+        int arraySize = random.nextInt(MAX_ARRAY_SIZE) + MIN_ARRAY_SIZE;
+        int filledIndex = random.nextInt(arraySize - 1);
+        MapEvent[] mapEvents = new MapEvent[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            if (i < filledIndex) {
+                MapEvent nullEvent = new MapEventSpy();
+                mapEvents[i] = nullEvent;
+            } else if (i == filledIndex) {
+                mapEvents[i] = expectedMapEvent;
+            } else {
+                MapEventSpy notNullEvent = new MapEventSpy();
+                notNullEvent.setInteractAction(new ActionSpy());
+                mapEvents[i] = notNullEvent;
+            }
+        }
+
+        return mapEvents;
     }
 }
